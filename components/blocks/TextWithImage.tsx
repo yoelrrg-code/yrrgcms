fe06c8f7584efce7d6968410fcc0d9fbfc1e86ea
Yoelkys R Rodriguez Gonzalez <yoel.rrg@gmail.com>
2026-08-04 11:17:23 -0300

    feat: set eager loading for TextWithImage component images

--- components/blocks/TextWithImage.tsx
+++ components/blocks/TextWithImage.tsx
@@ -27,6 +27,7 @@ export default function TextWithImage({
         src={imageUrl}
         alt={imageAlt}
         fill
+        loading="eager"
         className="object-cover transition-transform duration-700 group-hover:scale-105"
         sizes="(max-width: 768px) 100vw, 50vw"
       />
